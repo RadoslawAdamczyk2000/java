6b3e065d32fe778f1494e59ee2833a44011ed1a7
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-19 14:46:33 +0100

    compile first app

---
+++ eduweb/kurs-programowania-w-javie/01-wstep/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld { // Stworzenie klasy o nazwie Hello World
+    public static void main(String [] args){ // W klasie tworzę metodę "main", która jest punktem wejścia do programu. Jest to standardowa metoda, która musi istnieć w każdym programie Javy
+        System.out.println("Hello World"); // W metodzie "main" używam metody "println" z klasy "System" do wyświetlania napisu "Hello World" w konsoli
+    }
+}
+
+// Nazwa pliku powinna odpowiadać nazwie klasy
