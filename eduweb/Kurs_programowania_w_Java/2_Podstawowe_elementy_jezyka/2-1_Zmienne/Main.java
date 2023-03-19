b34ce41d1bf82574074ef7c950184cc4d74b08ad
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-19 15:57:19 +0100

    correct root directory

--- eduweb/Kurs_programowania_w_Java/2_Podstawowe_elementy_jezyka/2-1_Zmienne/Main.java
+++
@@ -1,8 +0,0 @@
-package eduweb.kurs_programowania_w_java._2.podstawowe_elementy_jezyka._2_1.zmienne;
-
-public class Main {
-    public static void main(String[] args){
-        
-    }
-}
-
---
+++ eduweb/kursprogramowaniawjava/_2_podstawoweelementyjezyka/_2_1_zmienne/Main.java
@@ -0,0 +1,8 @@
+package eduweb.kursprogramowaniawjava._2_podstawoweelementyjezyka._2_1_zmienne;
+
+public class Main {
+    public static void main(String[] args){
+        
+    }
+}
+
