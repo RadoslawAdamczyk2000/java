d3d6e685e5fac8fb90b834bd7e4f51370e6215b1
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-19 16:34:52 +0100

    finish 2_1

--- eduweb/kursprogramowaniawjava/_2_podstawoweelementyjezyka/_2_1_zmienne/Main.java
+++ eduweb/kursprogramowaniawjava/_2_podstawoweelementyjezyka/_2_1_zmienne/Main.java
@@ -2,10 +2,11 @@ package eduweb.kursprogramowaniawjava._2_podstawoweelementyjezyka._2_1_zmienne;
 
 public class Main {
     public static void main(String[] args){
-        byte value = 10; // definicja zmiennej byte - typ, oraz value - nazwa, 10 wartość
+        byte value = 100; // definicja zmiennej byte - typ, oraz value - nazwa, 10 wartość
         System.out.println(value);
         System.out.println("Value: " + value); // konkatenacja
-
+        float shortValue = 2.5f;
+        System.out.println("Short Value: " + shortValue); // konkatenacja
     }
 }
 
