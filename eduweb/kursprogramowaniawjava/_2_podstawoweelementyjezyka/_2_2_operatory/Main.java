e868aec344bfe1e4eb1d77c9f69df3a3eaeb5339
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-19 20:00:07 +0100

    operators lesson

---
+++ eduweb/kursprogramowaniawjava/_2_podstawoweelementyjezyka/_2_2_operatory/Main.java
@@ -0,0 +1,14 @@
+package eduweb.kursprogramowaniawjava._2_podstawoweelementyjezyka._2_2_operatory;
+
+public class Main {
+    public static void main(String[] args){
+        float a=2.0f, b=3.0f;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("a = a + b: " +( a+ b));
+        System.out.println("a = a - b: " +( a-b));
+        System.out.println("a = a * b: " +( a*b));
+        System.out.println("a = a / b: " +( a/b));
+        // System.out.println("a = a % b: " +( a&b));
+    }
+}
