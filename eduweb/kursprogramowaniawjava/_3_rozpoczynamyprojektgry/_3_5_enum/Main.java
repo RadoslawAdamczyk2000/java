42a8a016d3cc7a685dec75ba0ad221e8dee0ac4e
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-20 21:15:07 +0100

    copy/paste from create board lesson

--- eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_5_enum/Main.java
+++ eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_5_enum/Main.java
@@ -2,6 +2,51 @@ package eduweb.kursprogramowaniawjava._3_rozpoczynamyprojektgry._3_5_enum;
 
 public class Main {
     public static void main(String[] args){
-        
+        char [][] board = new char[10][10];
+        fillBoard(board);
+        printLetter();
+        printBoard(board);
+        fillBoard(board);
+        printLetter();
+        printBoard(board);
+    }
+
+    static void printLetter(){
+        System.out.print("  ");
+        for(int i = 0; i < 10; i++){
+            System.out.print((char)('A' + i));
+        }
+        System.out.print('\n');
+    }
+
+    static void printBoard(char[][] board){
+        for(int i = 0; i < 10; i++){
+            int numberToPrint = i + 1;
+            if(numberToPrint < 10){
+                System.out.print(' ');
+            }
+            System.out.print(numberToPrint);
+            for(int j = 0;j < 10; j++){
+                char shipValue = board[i][j];
+                System.out.print(shipValue);
+            }
+            System.out.print('\n');
+        }
+    }
+
+    private static void fillBoard(char[][] board){
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                board[i][j] = getRandomShip(Math.random());
+            }   
+        }
+    }
+
+    private static char getRandomShip(double ships){
+        if(ships < 0.2){
+            return '0';
+        }else{
+            return ' ';
+        }
     }
 }
