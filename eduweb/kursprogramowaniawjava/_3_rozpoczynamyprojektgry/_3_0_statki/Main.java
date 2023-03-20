dbd1bf9f714cffb420c11147a13b4d63fd4a3cf5
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-20 12:43:01 +0100

    create private method

--- eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_0_statki/Main.java
+++ eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_0_statki/Main.java
@@ -2,36 +2,39 @@ package eduweb.kursprogramowaniawjava._3_rozpoczynamyprojektgry._3_0_statki;
 
 public class Main {
     public static void main(String[] args){
-        topLabel(10);
+        printLetters(10);
         System.out.print('\n'); 
-        board(10);
+        printBoard(10);
     }
 
-    static void topLabel(int size){
+    static void printLetters(int size){
         System.out.print("  "); 
         for(int i=0; i < size; i++){
             System.out.print((char)('A' + i)); 
 
         }
     }
-    static void board(int size){
+    static void printBoard(int size){
         for(int i=1; i <= 10; i++){
             if(i < 10){
                 System.out.print(' '); 
             }
             System.out.print(i); 
-            ships();
+            for(int j=0;j<10;j++){
+                char shipValue = getRandomShip();
+               System.out.print(shipValue);
+            }
             System.out.println('\n'); 
 
         }
     }
-    static void ships(){
-        for(int j=0;j<10;j++){
-            if(Math.random() < 0.2){
-                System.out.print('O');
-            }else{
-                System.out.print(' ');
-            }
+
+    private static char getRandomShip() {
+        if(Math.random() < 0.2){
+            return '0';
+        }else{
+            return ' ';
         }
     }
+   
 }
