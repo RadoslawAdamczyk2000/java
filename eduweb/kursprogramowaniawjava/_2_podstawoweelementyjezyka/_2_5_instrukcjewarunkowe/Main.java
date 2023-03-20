5a6b6e962b9316f53bae2c8f78cd9904b0d86f9f
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-20 07:38:27 +0100

    compiled

---
+++ eduweb/kursprogramowaniawjava/_2_podstawoweelementyjezyka/_2_5_instrukcjewarunkowe/Main.java
@@ -0,0 +1,18 @@
+package eduweb.kursprogramowaniawjava._2_podstawoweelementyjezyka._2_5_instrukcjewarunkowe;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Wpisz wartość a: ");
+        int a = new Scanner(System.in).nextInt();
+        if(a == 3){
+            System.out.println("A jest równe 3");
+        }else if(a > 3){
+            System.out.println("A jest większe od 3");
+        }else{
+            System.out.println("A jest mniejsze od 3");
+        }
+    }
+}
