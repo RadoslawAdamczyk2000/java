0d98e21e1775f0fdc374386b2f535132e9742569
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-20 12:38:04 +0100

    split methods

--- eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_0_statki/Main.java
+++ eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_0_statki/Main.java
@@ -2,35 +2,36 @@ package eduweb.kursprogramowaniawjava._3_rozpoczynamyprojektgry._3_0_statki;
 
 public class Main {
     public static void main(String[] args){
-        // Narsowanie planszy
+        topLabel(10);
+        System.out.print('\n'); 
+        board(10);
+    }
+
+    static void topLabel(int size){
         System.out.print("  "); 
-        for(int i=0; i < 10; i++){
+        for(int i=0; i < size; i++){
             System.out.print((char)('A' + i)); 
-            // Jak to działa?
-            // System.out.println('A' + 1); Zwraca 6666... ponieważ 'A' jest kompliowane na ciąg liczb
-            //  Aby temu zapobiec stosuje się przekompliowanie na char, czyli pojednyczy znak
-            // System.out.println((char)'A'); 
 
         }
-        System.out.print('\n'); 
+    }
+    static void board(int size){
         for(int i=1; i <= 10; i++){
             if(i < 10){
                 System.out.print(' '); 
             }
             System.out.print(i); 
-            // Dodawanie wartości
-            for(int j=0;j<10;j++){
-                //  Random
-                if(Math.random() < 0.2){
-                    System.out.print('O');
-                }else{
-                    System.out.print(' ');
-                }
-            }
+            ships();
             System.out.println('\n'); 
 
         }
-
-        
+    }
+    static void ships(){
+        for(int j=0;j<10;j++){
+            if(Math.random() < 0.2){
+                System.out.print('O');
+            }else{
+                System.out.print(' ');
+            }
+        }
     }
 }
