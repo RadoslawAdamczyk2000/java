37b2720dd5801e8054f18857a0670057105c15d3
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-21 11:13:12 +0100

    finish contructors

--- eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_7_konstruktory/Main.java
+++ eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_7_konstruktory/Main.java
@@ -6,16 +6,26 @@ public class Main {
         double real;
         double img;
 
+        Complex(double r, double i){
+            real = r;
+            img = i;
+        }
+
         void show(){
             System.out.println("Real: " + real + ". Img: " + img);
+        }
+
+        Complex add(Complex z) {
+            return new Complex(real + z.real, img + z.img);
         }
     }
 
     public static void main(String[] args){
-        Complex z1 = new Complex();
-        Complex z2 = new Complex();
-        z1.real = 2.5;
-        z2.real = 5.5;
+        Complex z1 = new Complex(2.5,-3.6);
+        Complex z2 = new Complex(-2.5,3.6);
         z1.show();
+        z2.show();
+        Complex sum = z1.add(z2);
+        sum.show();
     }
 }
