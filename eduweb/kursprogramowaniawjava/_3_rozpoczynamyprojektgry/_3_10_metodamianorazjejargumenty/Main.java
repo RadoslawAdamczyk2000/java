a8377a50f16746a502ef792e680bb53dc9f23c56
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-21 11:57:18 +0100

    initial

---
+++ eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_10_metodamianorazjejargumenty/Main.java
@@ -0,0 +1,7 @@
+package eduweb.kursprogramowaniawjava._3_rozpoczynamyprojektgry._3_10_metodamianorazjejargumenty;
+
+public class Main {
+    public static void main(String[] args){
+        
+    }
+}
