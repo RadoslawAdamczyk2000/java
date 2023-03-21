8cac6467c76b3deef4dcd3ce3105ffccf57667b8
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-03-21 10:55:10 +0100

    liczby zespolone

--- eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_6_klasyiobiekty/Main.java
+++ eduweb/kursprogramowaniawjava/_3_rozpoczynamyprojektgry/_3_6_klasyiobiekty/Main.java
@@ -1,7 +1,13 @@
 package eduweb.kursprogramowaniawjava._3_rozpoczynamyprojektgry._3_6_klasyiobiekty;
 
 public class Main {
+
+    static class Complex {
+        double real;
+        double img;
+    }
+
     public static void main(String[] args){
-        
+        Complex z1 = new Complex();
     }
 }
